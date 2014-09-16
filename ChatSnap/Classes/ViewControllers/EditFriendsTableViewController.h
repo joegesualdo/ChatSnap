146c6de413e41e880cba815e1b6db92b79a17b11
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-16 16:32:47 -0400

    Create an empty edit friends view and controller

---
+++ ChatSnap/Classes/ViewControllers/EditFriendsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditFriendsTableViewController.h
+//  ChatSnap
+//
+//  Created by Joe Gesualdo on 9/16/14.
+//  Copyright (c) 2014 joegesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsTableViewController : UITableViewController
+
+@end
---
+++ ChatSnap/Classes/ViewControllers/EditFriendsTableViewController.m
@@ -0,0 +1,34 @@
+//
+//  EditFriendsTableViewController.m
+//  ChatSnap
+//
+//  Created by Joe Gesualdo on 9/16/14.
+//  Copyright (c) 2014 joegesualdo. All rights reserved.
+//
+
+#import "EditFriendsTableViewController.h"
+
+@interface EditFriendsTableViewController ()
+
+@end
+
+@implementation EditFriendsTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+@end
